9b7cf96d0f9f6faf956f05ae87621ffcc9f24464
ifCoder <ifcoderweb@gmail.com>
2025-12-10 21:43:13 -0300

    configurações iniciais e estrutura do site

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 8081
+
+// Configs
+    // View Engine
+    app.set('view engine', 'ejs');
+
+    // Static
+    app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render("index")
+})
+
+app.listen(port, () => {
+  console.log(`No ar! Porta: ${port}`)
+})
